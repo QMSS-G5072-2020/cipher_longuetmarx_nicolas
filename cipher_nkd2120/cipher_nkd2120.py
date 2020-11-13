5b80341674bceb6321e1d5476f978f10a2cad5a1
Nicolas Longuet Marx <nkd2120@columbia.edu>
2020-11-13 18:39:59 -0500

    documenting the function

--- cipher_nkd2120/cipher_nkd2120.py
+++ cipher_nkd2120/cipher_nkd2120.py
@@ -11,3 +11,26 @@ def cipher(text, shift, encrypt=True):
             new_text += alphabet[new_index:new_index+1]
     return new_text
 
+ """
+    Encodes the secret message that has been entered by the user. 
+    VERY CONFIDENTIAL!
+
+    Parameters
+    ----------
+    text : The initial message you want to encrypt
+      A string. 
+    shift : The number of letter you want to move from. 
+        A float. 
+
+    Returns
+    -------
+    new_text: the encrypted message. 
+
+    Examples
+    --------
+    >>> from cipher_nkd2120 import cipher
+    >>> my_message ="I have secret information"
+    >>> my_ket = 3
+    >>> cipher(my_message, my_key)
+
+    """
